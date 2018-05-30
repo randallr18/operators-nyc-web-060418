9fe664ff17c9eaa5122e111d28f93a36e9f5806c
Risher Randall <randallr18@mail.wlu.edu>
2018-05-30 23:36:27 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-speed > 60 ? true : false
+speed > 60 || speed < 40 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60 || speed < 60
+    true
+  else
+    false
+  end
 end
 	
 
